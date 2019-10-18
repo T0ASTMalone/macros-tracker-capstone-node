b6676efba5e556857ac0a81d24ffdc645153ebc7
Miguel Ponce <miguelaponce258@gmail.com>
2019-10-17 18:03:43 -0600

    updated foods endpoint to format foods before sending foods to client

--- src/foods/foods-router.js
+++ src/foods/foods-router.js
@@ -9,12 +9,13 @@ foodsRouter
   .route('/')
   .all(requireAuth)
   .get(jsonParser, (req, res, next) => {
-    const userId = req.body.user_id;
+    const userId = req.user.user_id;
     foodsServices
       .getUserFoods(req.app.get('db'), userId)
-      .then(foods =>
-        res.json(foods.map(food => foodsServices.serializeFood(food)))
-      )
+      .then(foods => {
+        foodsServices.formatFoods(foods);
+        return res.json(foods.map(food => foodsServices.serializeFood(food)));
+      })
       .catch(next);
   })
   .post(jsonParser, (req, res, next) => {
