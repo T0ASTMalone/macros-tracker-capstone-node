095ff634313efdf2aac5b5dd7febe618683093cc
Miguel Ponce <miguelaponce258@gmail.com>
2019-10-17 19:40:48 -0600

    preparing for deployment

--- postgrator-production-config.js
+++ postgrator-production-config.js
@@ -7,7 +7,7 @@ module.exports = {
     process.env.NODE_ENV === 'test'
       ? process.env.TEST_DATABASE_URL
       : process.env.DATABASE_URL,
-  ssl: true,
+  ssl: !!process.env.SSL,
   host: process.env.PROD_MIGRATION_DB_HOST,
   port: process.env.PROD_MIGRATION_DB_PORT,
   database: process.env.PROD_MIGRATION_DB_NAME,
