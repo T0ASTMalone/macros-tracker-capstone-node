22ed722f31575d80bf1a242f665a8c6ed6f932b1
Miguel Ponce <miguelaponce258@gmail.com>
2019-10-17 18:49:40 -0600

    updated expiry time

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -40,7 +40,8 @@ authRouter.post('/refresh', requireAuth, (req, res, next) => {
   const sub = req.user.email;
   const payload = { user_id: req.user.user_id };
   res.send({
-    authToken: AuthService.createJwt(sub, payload)
+    authToken: AuthService.createJwt(sub, payload),
+    payload
   });
 });
 
